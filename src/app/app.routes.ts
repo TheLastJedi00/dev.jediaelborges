6b7c486be691bd27512349aaf2920b5702cef433
Jediael Borges <jediaelborges23@gmail.com>
2025-07-12 12:04:55 -0300

    Currículo is finish

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { CurriculumComponent } from './pages/curriculum/curriculum.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'about', component: AboutComponent}
+    {path: 'about', component: AboutComponent},
+    {path: 'curriculum', component: CurriculumComponent}
 ];
---
+++ src/app/pages/curriculum/curriculum.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
+
+@Component({
+  selector: 'app-curriculum',
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent, CommonModule],
+  templateUrl: './curriculum.component.html',
+  styleUrl: './curriculum.component.scss',
+})
+export class CurriculumComponent {
+  hardSkills: string[] = [
+    'Java & Spring',
+    'TypeScript & Angular',
+    'Git & GitHub',
+    'Kanban & Scrum',
+    'Testes Unitários',
+    'JavaScript & React',
+    'English B1'
+  ];
+softSkills: string[] =[
+  'Pensamento crítico',
+  'Adaptabilidade a mudanças',
+  'Aberto a feedback',
+  'Aprendizado contínuo',
+  'Gestão de tempo',
+  'Escuta ativa',
+  'Comunicação clara',
+];
+progressSkills: string[] = [
+  'Kotlin & Spring',
+  'NextJS',
+  'English B2'
+  ];
+}
